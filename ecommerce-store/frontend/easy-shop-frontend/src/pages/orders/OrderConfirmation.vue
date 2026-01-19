a1d28b64eef658c62166133a4d6a8a23903debdb
Kingsley <kifeanyi216@gmail.com>
2026-01-19 12:04:11 +0100

    AWS S3 Bucket Setup

---
+++ ecommerce-store/frontend/easy-shop-frontend/src/pages/orders/OrderConfirmation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="order-confirmation">
+    <h1>Order Confirmation</h1>
+    <p>Your order has been successfully placed.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
